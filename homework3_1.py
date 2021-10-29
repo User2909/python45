ba1c2db9f0ca692c2493ca223a5610e667e57a09
User2909 <rct.savinykh@gmail.com>
2021-10-29 18:10:31 +0300

    fix and change

--- homework3_1.py
+++ homework3_1.py
@@ -2,7 +2,7 @@ print('enter two something ')
 no1=input()
 no2=input()
 
-if no1.isdigit() and no2.isdigit():# type(no1)==float and (type(no2)==int or type(no2)==float)
+if no1.isdigit() and no2.isdigit():
     if (float(no1) >= 3 and float(no1) <= 21) and (float(no2) >= 3 and float(no2) <= 21):
         print(abs(float(no1)-float(no2)))
     else:
